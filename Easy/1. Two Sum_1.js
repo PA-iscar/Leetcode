5017f139939a644e1e39653188c64c017fc732bd
Abhishek <abhiaro27@gmail.com>
2022-06-01 13:44:51 +0530

    Two Sum first bruteforce approach

---
+++ Easy/1. Two Sum_1.js
@@ -0,0 +1,25 @@
+// ! 1. Two Sum Brute force approach
+
+// *  Time complexity:- O(n^2)
+// *  Space complexity:- O(1)
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+	for (let i = 0; i < nums.length; i++) {
+		for (let j = i + 1; j < nums.length; j++) {
+			if (nums[i] + nums[j] === target) {
+				return [i, j];
+			}
+		}
+	}
+};
+
+console.log(twoSum([2, 7, 11, 15], 9));
+
+console.log(twoSum([3, 2, 4], 6));
+
+console.log(twoSum([3, 3], 6));
